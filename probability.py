9f309ccb7bf7e8832c1cb9d79eaf2b4afcb95fe8
AD1024 <ccoderad@gmail.com>
2017-12-06 09:45:39 +0800

    add: probability lib

---
+++ probability.py
@@ -0,0 +1,46 @@
+import statkit.basic as basic
+import math
+
+
+@basic.list_check()
+def expected(_x, _p):
+    if len(_x) != len(_p):
+        return basic.nan
+    return sum(map(lambda i: _x[i] * _p[i], range(len(_x))))
+
+
+@basic.list_check()
+def var(_x, _p):
+    if len(_x) != len(_p):
+        return basic.nan
+    mu = expected(_x, _p)
+    return sum(map(lambda i: _p[i] * (_x[i] - mu) ** 2), range(len(_x)))
+
+
+class Dist:
+    @classmethod
+    def norm_pd(cls, _x, sd, mu):
+        return (1 / (sd * math.sqrt(2 * math.pi))) * (math.e ** (-((_x - mu) ** 2) / (2 * (sd ** 2))))
+
+    @classmethod
+    def _cdf(cls, x, sd, mu):
+        return 0.5 * (1 + math.erf((x - mu) / (sd * math.sqrt(2))))
+
+    @classmethod
+    def norm_cd(cls, sd, mu, lower=-math.inf, upper=math.inf):
+        lower, upper = (lower, upper) if lower <= upper else (upper, lower)
+        return cls._cdf(upper, sd, mu) - cls._cdf(lower, sd, mu)
+
+    @classmethod
+    def plot_norm_curve(cls, sd, mu):
+        try:
+            import matplotlib.pyplot as plt
+            import numpy as np
+        except ImportError:
+            raise ImportError('mathplotlib is not installed')
+        _x = np.linspace(mu - 4 * sd, mu + 4 * sd + 1, 100)
+        func = lambda x: (1 / (sd * math.sqrt(2 * math.pi))) * (math.e ** (-((x - mu) ** 2) / (2 * (sd ** 2))))
+        _y = list(map(func, _x))
+        plt.plot(_x, _y)
+        plt.show()
+        plt.close()
